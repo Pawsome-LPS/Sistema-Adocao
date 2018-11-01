4d481fc1a6f81e002741d85819b48d3454921817
Luizgustavo358 <luiz.braganca@sga.pucminas.br>
2018-11-01 12:08:26 -0300

    Pessoa Cadastro

--- website/src/components/PessoaCadastro.js
+++ website/src/components/PessoaCadastro.js
@@ -1,23 +1,35 @@
 import React from 'react';
+import { Container, Row, Col, Input, Button, Fa, Card, CardBody } from 'mdbreact';
 
-class PessoaCadastro extends React.Component {
-
-    render() {
-        return(
-            <div className="card">
-                <h2 className="cadastre">Cadastre-se</h2>
+class PessoaCadastro extends React.Component  {
+  render() {
+    return(
+      <Container>
+        <Row>
+          <Col md="6">
+            <Card>
+              <CardBody>
                 <form>
-                    <input id="nome" type="text" className="form-control" name="nome" placeholder="Nome" />
-                    <input id="email" type="text" className="form-control" name="email" placeholder="Email" />
-                    <input id="telefone" type="number" className="form-control" name="telefone" placeholder="Telefone" />
-                    <input id="password" type="password" className="form-control" name="password" placeholder="Senha" />
-                    <input id="confirm-password" type="password" className="form-control" name="confirm-password" placeholder="Confirme a Senha" />
-                    <input className="btn btn-primary" type="file" accept="image/gif, image/jpeg, image/png" /><br />
-                    <input className="btn btn-primary" type="submit" value="Cadastrar" /><br />
+                  <p className="h4 text-center py-4 cadastre">Cadastre-se</p>
+                  <div className="grey-text">
+                    <Input label="Nome" icon="user" group type="text" validate error="wrong" success="right"/>
+                    <Input label="Email" icon="envelope" group type="email" validate error="wrong" success="right"/>
+                    <Input label="Telefone" icon="phone" group type="number" validate error="wrong" success="right"/>
+                    <Input label="Senha" icon="lock" group type="password" validate/>
+                    <Input label="Confirme a senha" icon="exclamation-triangle" group type="password" validate error="wrong" success="right"/>
+                  </div>
+                  <div className="text-center py-4 mt-3">
+                    <Button color="red">Cancelar</Button>
+                    <Button color="cyan" type="submit">Register</Button>
+                  </div>
                 </form>
-            </div>
-        )
-    }
-}
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+};
 
 export default PessoaCadastro;
