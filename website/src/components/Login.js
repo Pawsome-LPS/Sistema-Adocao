743252caf38e617e127903819d72958bdca72152
Luizgustavo358 <luiz.braganca@sga.pucminas.br>
2018-11-01 12:27:25 -0300

    componente Login

--- website/src/components/Login.js
+++ website/src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container, Row, Col, Input, Button } from 'mdbreact';
+import { Container, Row, Col, Input, Button, Fa, Card, CardBody } from 'mdbreact';
+import 'mdbreact/dist/css/mdb.css'; 
 
 class Login extends React.Component {
     render() {
@@ -7,19 +8,23 @@ class Login extends React.Component {
             <Container>
                 <Row>
                     <Col md="6">
-                        <form>
-                            <p className="h5 text-center mb-4">Login</p>
-                            <div className="grey-text">
-                                <Input label="Email" icon="envelope" group type="email" validate error="wrong" success="right" />
-                                <Input label="Senha" icon="lock" group type="password" validate />
-                            </div>
-                            <div className="text-center">
-                                <Button>Login</Button>
-                            </div>
-                        </form>
-                        <p>
-                            <a href="#">Não possui login? Cadastre-se aqui</a>
-                        </p>
+                        <Card>
+                            <CardBody>
+                                <form>
+                                    <p className="h5 text-center mb-4 cadastre">Login</p>
+                                    <div className="grey-text">
+                                        <Input label="Email" icon="envelope" group type="email" validate error="wrong" success="right" />
+                                        <Input label="Senha" icon="lock" group type="password" validate />
+                                    </div>
+                                    <div className="text-center">
+                                        <Button>Login</Button>
+                                    </div>
+                                </form>
+                                <p className="cadastre">Não possui login?
+                                    <a href="#"> Cadastre-se aqui</a>
+                                </p>
+                            </CardBody>
+                        </Card>
                     </Col>
                 </Row>
             </Container>
