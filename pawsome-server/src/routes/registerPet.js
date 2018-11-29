7bdad8261bcd06ebdc46aebbc993c62fbebf070d
Arthur L Pereira <gloriouslordbear@outlook.com>
2018-11-29 12:06:27 +0000

    cadastro pet

--- pawsome-server/src/routes/registerPet.js
+++ pawsome-server/src/routes/registerPet.js
@@ -1,20 +1,25 @@
 import express from 'express';
 const router = express.Router();
 router.post('/',  (req, res, next) => {
-    email = req.body.email;
-    password = req.body.password;
+    o_id = req.body.owner_id;
     nome = req.body.nome;
+    raca = req.body.raca;
 
     var status;
     MongoClient.connect("mongodb://localhost:27017/pawsome", (err, db) => {
+        if (err) {
+            status = null;
+            return;
+        }
         collection = db.collection('users');
-        collection.insertOne({email: email, password: password, name: name}, (error, item) => {
-            status = item ?  "Ok" : "";
+
+        collection.insertOne({owner_id: o_id, nome: nome, raca: raca}, (error, res) => {
+            status = error ? null : "ok";
         })
     })
     if (status) {
         res.status(200).send({
-            title: "Register!"
+            title: "Register Pet!"
         });
     }
     else {
